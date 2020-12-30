a07bf92f357cab647f73195425726be9a65d1704
Ryan <r@goodestlab.com>
2020-12-30 19:25:45 +0800

    nice

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['isAuth'] = require('../middleware/isAuth.js')
-middleware['isAuth'] = middleware['isAuth'].default || middleware['isAuth']
-
-export default middleware
