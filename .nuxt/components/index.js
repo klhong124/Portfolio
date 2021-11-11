7db5f199269891f69c3b44c0951ab6d907b86285
Ryan Kwan (Mac mini) <klhong124@gmail.com>
2021-11-11 18:32:05 +0000

    init next

--- .nuxt/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Job } from '../../components/Job.vue'
-
-export const LazyJob = import('../../components/Job.vue' /* webpackChunkName: "components/Job" */).then(c => c.default || c)
