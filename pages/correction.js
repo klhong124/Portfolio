4f8e7c025c9abc62c89f5bf310cb4bc799e17b9b
Ryan Kwan (Mac mini) <klhong124@gmail.com>
2021-12-13 17:52:00 +0000

    Update correction.js

--- pages/correction.js
+++ pages/correction.js
@@ -1,24 +1,24 @@
-import React, { useEffect, useRef, useState } from "react";
-import $ from 'jquery';
+import React, { useState } from "react";
 
-
+const dictionary = {
+    "realy": "really",
+    "wried": "weird"
+}
 
 const index = () => {
-    const [text,setText] = useState(null)
-    useEffect(() => {
-        setText(text.replace("realy","really"))
-    }, [textarea]);
-
-
+    const [text, setText] = useState("")
+    const onChange = (input) => {
+        let text = input
+        for (const [wrong, right] of Object.entries(dictionary)) {
+            text = text.replace(wrong, right)
+        }
+        setText(text)
+    }
     return (
         <div>
-            <Head>
-                <title>{meta.title}</title>
-            </Head>
-
-
-            <div className="dark bg-crayola">
-                <textarea value={text}></textarea>
+            <div className="dark h-screen bg-blue-200">
+                <h1>auto correct here: (try "realy" | "wried" )</h1>
+                <input value={text} onChange={event => onChange(event.target.value)} />
             </div>
         </div>
 
